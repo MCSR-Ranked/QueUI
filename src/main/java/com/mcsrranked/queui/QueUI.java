13de49545290e490f676e155d30d609994fe0e48
RedLime <redlimerl@gmail.com>
2025-10-26 04:39:33 +0900

    INIT

---
+++ src/main/java/com/mcsrranked/queui/QueUI.java
@@ -0,0 +1,10 @@
+package com.mcsrranked.queui;
+
+import net.fabricmc.api.ModInitializer;
+
+public class QueUI implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
